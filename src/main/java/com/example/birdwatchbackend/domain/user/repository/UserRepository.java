7876e4c0c124b9d8cabf812e8a1d284bba012ba6
Radoslaw-Wolnik <radek.wolnik@gmail.com>
2026-05-05 22:26:13 +0200

    Create UserRepository.java

---
+++ src/main/java/com/example/birdwatchbackend/domain/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.birdwatchbackend.domain.user.repository;
+
+import com.example.birdwatchbackend.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+}
