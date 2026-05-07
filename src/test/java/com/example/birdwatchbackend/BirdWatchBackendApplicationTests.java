d060a7f8a6619261789d96fb55dcea46f20d9a46
Radoslaw-Wolnik <radek.wolnik@gmail.com>
2026-05-07 14:19:27 +0200

    made integration tests for user and auth services and controllers

--- src/test/java/com/example/birdwatchbackend/BirdWatchBackendApplicationTests.java
+++ src/test/java/com/example/birdwatchbackend/BirdWatchBackendApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BirdWatchBackendApplicationTests {
+class BirdWatchBackendApplicationTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {
     }
-
 }
